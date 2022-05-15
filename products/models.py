07968a29a7fb2e5e0fc9fefe69f24e3b4ad8c54d
ip69719 <ipeters@mail.com>
2022-05-15 23:06:40 +0000

    Created Products model

--- products/models.py
+++ products/models.py
@@ -12,3 +12,17 @@ class Category(models.Model):
     def get_friendly_name(self):
         """ Model method to return friendly name """
         return self.friendly_name
+
+
+class Product(models.Model):
+    """ Product model """
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL
+        )
+    name = models.CharField(max_length=254)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
