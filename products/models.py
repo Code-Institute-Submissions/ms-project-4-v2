e68f0d6317e1264c630ee812c55ec62cc3738cb7
ip69719 <ipeters@mail.com>
2022-05-15 22:54:47 +0000

    Created Category model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """ Category model """
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        """ Model method to return friendly name """
+        return self.friendly_name
