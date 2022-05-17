73aaa95c3f58c99fb8348812cd4a2eb70244cef7
ip69719 <ipeters@mail.com>
2022-05-17 23:52:53 +0000

    Customised admin

--- products/admin.py
+++ products/admin.py
@@ -3,5 +3,22 @@ from .models import Category, Product
 
 # Register your models here.
 
-admin.site.register(Category)
-admin.site.register(Product)
+class CategoryAdmin(admin.ModelAdmin):
+    """ Customised admin to add fields """
+    list_display = (
+        'name',
+        'friendly_name',
+    )
+
+class ProductAdmin(admin.ModelAdmin):
+    """ Customised admin to add fields """
+    list_display = (
+        'name',
+        'category',
+        'description',
+        'price',
+        'image',
+    )
+
+admin.site.register(Category,CategoryAdmin)
+admin.site.register(Product,ProductAdmin)
