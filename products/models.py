b6de13c04ef75fa8fc957615f7c586f2fe258e86
ip69719 <ipeters@mail.com>
2022-05-17 08:26:16 +0000

    Add description field to product model

--- products/models.py
+++ products/models.py
@@ -20,8 +20,8 @@ class Product(models.Model):
         'Category', null=True, blank=True, on_delete=models.SET_NULL
         )
     name = models.CharField(max_length=254)
+    description = models.CharField(max_length=254, default="No Description")
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
