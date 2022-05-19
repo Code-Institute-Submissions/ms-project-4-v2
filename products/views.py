14020365886e0670e10bc50625624dcc36bd0442
ip69719 <ipeters@mail.com>
2022-05-19 12:37:53 +0000

    Add filtering by category

--- products/views.py
+++ products/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 
 
 def all_products(request):
-    """ A view to show all menu options, including sorting """
+    """ A view to show all menu options, including product filtering """
 
     products = Product.objects.all()
+    categories = None
+
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
 
     context = {
         'products': products,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
