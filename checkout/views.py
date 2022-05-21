cd671c3a47fd223dd28712f6024c7b11e82f9f26
ip69719 <ipeters@mail.com>
2022-05-21 20:19:14 +0000

    Added Stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -17,6 +17,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51L1sCdJrZt59IP2KMILh8Wr2TsCGuriGspBbzeD2BBlzyjdQ44UKyZwxzqvrjVhVZQEdQsIltkLthfn4mHEtVR8Z00IXJM2YHD',
+        'client_secret': 'sk_test_51L1sCdJrZt59IP2KLiQLlNmc7cLhD2RBG3X3Oe5Rx2UOETeZulpic2IWUjReZ38HcwoDRxhdIvwhmU37jAt9X2HW00avw1HHBQ',
     }
 
     return render(request, template, context)
